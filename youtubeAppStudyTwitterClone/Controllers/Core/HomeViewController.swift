95f01aa92978a1476d16bcbe94c39cb7ea532827
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-11-24 19:28:39 +0500

    Add logic to VCs registration and login, Network update

--- youtubeAppStudyTwitterClone/Controllers/Core/HomeViewController.swift
+++ youtubeAppStudyTwitterClone/Controllers/Core/HomeViewController.swift
@@ -42,6 +42,12 @@ class HomeViewController: UIViewController {
         timelineTableView.delegate = self
         timelineTableView.dataSource = self
         configureNavigationBar()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "rectangle.portrait.and.arrow.right"), style: .plain, target: self, action: #selector(didTapSignOut))
+    }
+    
+    @objc private func didTapSignOut() {
+        try? Auth.auth().signOut()
+        handleAuthentication()
     }
     
     override func viewDidLayoutSubviews() {
@@ -49,15 +55,18 @@ class HomeViewController: UIViewController {
         timelineTableView.frame = view.frame
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = false
-        
+    private func handleAuthentication() {
         if Auth.auth().currentUser == nil {
             let vc = UINavigationController(rootViewController: OnboardingViewController())
             vc.modalPresentationStyle = .fullScreen
             present(vc, animated: false)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+        handleAuthentication()
     }
 }
 
--- youtubeAppStudyTwitterClone/Networking/AuthManager.swift
+++ youtubeAppStudyTwitterClone/Networking/AuthManager.swift
@@ -15,7 +15,13 @@ class AuthManager {
     static let shared = AuthManager()
     
     func registerUser(with email: String, password: String) -> AnyPublisher<User, Error> {
-        return Auth.auth().createUser(withEmail: email, password: password)
+        Auth.auth().createUser(withEmail: email, password: password)
+            .map(\.user)
+            .eraseToAnyPublisher()
+    }
+    
+    func loginUser(with email: String, password: String) -> AnyPublisher<User, Error> {
+        Auth.auth().signIn(withEmail: email, password: password)
             .map(\.user)
             .eraseToAnyPublisher()
     }
