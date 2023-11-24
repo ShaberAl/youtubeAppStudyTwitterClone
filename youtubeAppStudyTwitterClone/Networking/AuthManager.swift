a1e3f3e9efff2ce99ca17058750d3a3f3b4216d2
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-11-24 17:01:56 +0500

    Add users to Firebase

---
+++ youtubeAppStudyTwitterClone/Networking/AuthManager.swift
@@ -0,0 +1,22 @@
+//
+//  AuthManager.swift
+//  youtubeAppStudyTwitterClone
+//
+//  Created by MacOS on 24.11.2023.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuthCombineSwift
+import Combine
+
+class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    func registerUser(with email: String, password: String) -> AnyPublisher<User, Error> {
+        return Auth.auth().createUser(withEmail: email, password: password)
+            .map(\.user)
+            .eraseToAnyPublisher()
+    }
+}
